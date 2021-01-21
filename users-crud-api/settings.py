e3f025f6c46ee388af3f6728c6d4957e0f8850fd
Xenine <ivanmyazin99@gmail.com>
2021-01-21 03:46:07 +0300

    change key

--- users-crud-api/settings.py
+++ users-crud-api/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 import os
 from pathlib import Path
 import dj_database_url
-from .local_settings import SECRET_KEY
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,6 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
+SECRET_KEY = '2yx&bj+wfhxe6$w#-46098g^nheu&81m)yhy*-h8&fp&!61_l@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
