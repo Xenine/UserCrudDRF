3764c83674c606d0b30a163b6f02830be75a1aa8
Xenine <ivanmyazin99@gmail.com>
2021-01-21 04:06:53 +0300

    some changes

--- users-crud-api/settings.py
+++ users-crud-api/settings.py
@@ -125,7 +125,16 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.1/howto/static-files/
+# https://docs.djangoproject.com/en/1.11/howto/static-files/
+
+PROJECT_ROOT = os.path.join(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra lookup directories for collectstatic to find static files
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
 
 
 
