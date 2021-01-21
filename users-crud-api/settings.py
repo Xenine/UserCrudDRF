79d27302f94c67e6fc027247fa5567960ca92182
Xenine <ivanmyazin99@gmail.com>
2021-01-21 03:49:40 +0300

    change key3

--- users-crud-api/settings.py
+++ users-crud-api/settings.py
@@ -76,6 +76,7 @@ WSGI_APPLICATION = 'users-crud-api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+DATABASES = {}
 prod_db  =  dj_database_url.config(conn_max_age=600)
 DATABASES['default'].update(prod_db)
 
