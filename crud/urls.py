eb875e2543694e6e00b64b77f24ba5e3b668aacf
Xenine <ivanmyazin99@gmail.com>
2021-01-21 05:11:00 +0300

    change

--- crud/urls.py
+++ crud/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path, include
 from rest_framework.routers import SimpleRouter
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from . import view
 
