91b06b50db2710128444e232edc7558b5e93f270
Xenine <ivanmyazin99@gmail.com>
2021-01-22 18:37:49 +0300

    fix update / patch / put

--- crud/serializers.py
+++ crud/serializers.py
@@ -11,11 +11,13 @@ class UserSerializer(serializers.ModelSerializer):
         user.save()
         return user
 
-    def update(self, validated_data):
+    def update(self, instance, validated_data):
+        instance.username = validated_data.get('username', instance.username)
+        instance.first_name = validated_data.get('first_name', instance.first_name)
+        instance.last_name = validated_data.get('last_name', instance.last_name)
         password = validated_data.pop('password')
-        user = User(**validated_data)
         user.set_password(password)
-        user.update()
+        user.save()
         return user
 
     class Meta:
