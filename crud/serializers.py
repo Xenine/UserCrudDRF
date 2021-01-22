b464b4acb8d0a24a4615800f92141357ec36b87e
Xenine <ivanmyazin99@gmail.com>
2021-01-22 16:39:25 +0300

    fix update / patch

--- crud/serializers.py
+++ crud/serializers.py
@@ -11,6 +11,13 @@ class UserSerializer(serializers.ModelSerializer):
         user.save()
         return user
 
+    def update(self, validated_data):
+        password = validated_data.pop('password')
+        user = User(**validated_data)
+        user.set_password(password)
+        user.save()
+        return user
+        
     class Meta:
         model = User
         fields = [
