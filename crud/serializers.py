6350e9867a7ca628eb29c410b23aae481ea9a8fa
Xenine <ivanmyazin99@gmail.com>
2021-01-22 19:40:49 +0300

    fix update / patch / put

--- crud/serializers.py
+++ crud/serializers.py
@@ -12,16 +12,12 @@ class UserSerializer(serializers.ModelSerializer):
         return user
 
     def update(self, user, validated_data):            
-        try:
-            user.username = validated_data.get('username')
-            user.first_name = validated_data.get('first_name')
-            user.last_name = validated_data.get('last_name')
-            password = validated_data.get('password')
-            user.set_password(password)
-            user.save()
-        except:
-            return Response({'message': 'Необходимы все аргементы'})
-
+        user.username = validated_data.get('username')
+        user.first_name = validated_data.get('first_name')
+        user.last_name = validated_data.get('last_name')
+        password = validated_data.get('password')
+        user.set_password(password)
+        user.save()
         return user
 
     def partial_update(self, user, validated_data):
